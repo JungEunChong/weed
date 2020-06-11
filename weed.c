cb8ebd2458bcfd0a37f6c22b183b8300843fce8a
JungEunChong <21900673@handong.edu>
2020-06-12 06:44:12 +0900

    free - 2

--- weed.c
+++ weed.c
@@ -2,12 +2,23 @@
 
 int main() {
 	int a;
+	int b;
 	printf("a : ");
 	scanf("%d", &a);
 
 	for(int i = 0; i < a; i++) {
 		for(int j = a; j > i; j--) {
 			printf("*");
+		}
+		printf("\n");
+	}
+
+	printf("\nb : ");
+	scanf("%d", &b);
+
+	for(int i = 0; i < b; i++) {
+		for(int j = 0; j <= i; j++) {
+			printf("#");
 		}
 		printf("\n");
 	}
